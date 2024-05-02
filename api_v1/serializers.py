b63f9d0e82358606498e0c5bda40cdc14dd380f6
baka <denisfurdilov@gmail.com>
2024-05-02 10:21:53 +0700

    1

--- api_v1/serializers.py
+++ api_v1/serializers.py
@@ -4,11 +4,22 @@ from .models import *
  
 
 class ProjectSerializer(serializers.ModelSerializer):
+    photo = serializers.SerializerMethodField('get_image_url')
+
+    def get_image_url(self, obj):
+        return obj.photo.url
     class Meta:
         model = Project
         fields = ('photo', 'title', 'description', 'is_realized', 'type', 'slug')
 
+    
+
+
 class EmployeeSerializer(serializers.ModelSerializer):
+    photo = serializers.SerializerMethodField('get_image_url')
+
+    def get_image_url(self, obj):
+        return obj.photo.url
     class Meta:
         model = Employee
         fields = ('photo', 'post', 'last_name', 'first_name', 'patronymic', 'link')
@@ -25,11 +36,19 @@ class ContactSerializer(serializers.ModelSerializer):
 
 class PortfoloSerializer(serializers.ModelSerializer):
     authors = EmployeeSerializer(read_only=True, many=True)
+    photo = serializers.SerializerMethodField('get_image_url')
+
+    def get_image_url(self, obj):
+        return obj.photo.url
     class Meta:
         model = Project
         fields = ('photo', 'title', 'authors', 'description', 'instruction', 'is_realized', 'type', 'slug')
  
 class ArticleSerializer(serializers.ModelSerializer):
+    photo = serializers.SerializerMethodField('get_image_url')
+
+    def get_image_url(self, obj):
+        return obj.photo.url
     class Meta:
         model = Article
         fields = ('title', 'desc', 'text', 'slug', 'date', 'photo')
